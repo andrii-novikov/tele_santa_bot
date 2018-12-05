06fd4eff6f3db00a90372b190c2d934ecfb78139
andy1341 <andruha2921@gmail.com>
2018-12-05 09:04:31 +0200

    add complatej

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   default_url_options host: Rails.application.credentials[Rails.env.to_sym].dig(:default_host)
   telegram_webhook TelegramWebhooksController
+
+  root to: 'home#index'
+
+  resource :home, only: %i[index]
 end
